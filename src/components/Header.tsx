9edfdfd46686bb9ee49383ebbb68e842b95b262b
aRav3n <24660772+aRav3n@users.noreply.github.com>
2026-03-07 07:35:05 +0530

    build: add link and update styling for Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
 import "../styles/Header.css";
 
 export function Header() {
   return (
     <header>
-      <img src="./logo.png" alt="logo" />
+      <Link to="/">
+        <img src="./logo.png" alt="logo" />
+      </Link>
       <div>
         <h1>Global Harmony</h1>
         <p>Scheduling Made Seamless Across Time Zones</p>
