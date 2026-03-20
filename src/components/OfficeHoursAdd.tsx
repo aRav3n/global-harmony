3cab5413881503063c6d3c57d2eead0133a112ce
aRav3n <24660772+aRav3n@users.noreply.github.com>
2026-03-20 15:01:49 +0530

    style: update placement of office hours section

--- src/components/OfficeHoursAdd.tsx
+++ src/components/OfficeHoursAdd.tsx
@@ -20,40 +20,7 @@ export function OfficeHoursAdd({
   };
 
   return (
-    <div>
-      {attendee.officeHours.map((officeHourBlock) => {
-        return (
-          <div
-            key={`office-hours-${officeHourBlock.id}`}
-            className="office-hour-block"
-          >
-            <label htmlFor={`${officeHourBlock.id}-start-time`}>
-              Start Time
-              <input
-                id={`${officeHourBlock.id}-start-time`}
-                type="time"
-                name="start"
-                value={officeHourBlock.start}
-                onChange={(e) => {
-                  updateOfficeHours(e.target.value, officeHourBlock.id, true);
-                }}
-              />
-            </label>
-            <label htmlFor={`${officeHourBlock.id}-end-time`}>
-              End Time
-              <input
-                id={`${officeHourBlock.id}-end-time`}
-                type="time"
-                name="end"
-                value={officeHourBlock.end}
-                onChange={(e) => {
-                  updateOfficeHours(e.target.value, officeHourBlock.id, false);
-                }}
-              />
-            </label>
-          </div>
-        );
-      })}
+    <div className="office-hours-container">
       <button
         type="button"
         onClick={() => {
@@ -66,6 +33,45 @@ export function OfficeHoursAdd({
           : null}{" "}
         Office Hours
       </button>
+      <div className="office-hours-section">
+        {attendee.officeHours.map((officeHourBlock) => {
+          return (
+            <div
+              key={`office-hours-${officeHourBlock.id}`}
+              className="office-hour-block"
+            >
+              <label htmlFor={`${officeHourBlock.id}-start-time`}>
+                Start Time
+                <input
+                  id={`${officeHourBlock.id}-start-time`}
+                  type="time"
+                  name="start"
+                  value={officeHourBlock.start}
+                  onChange={(e) => {
+                    updateOfficeHours(e.target.value, officeHourBlock.id, true);
+                  }}
+                />
+              </label>
+              <label htmlFor={`${officeHourBlock.id}-end-time`}>
+                End Time
+                <input
+                  id={`${officeHourBlock.id}-end-time`}
+                  type="time"
+                  name="end"
+                  value={officeHourBlock.end}
+                  onChange={(e) => {
+                    updateOfficeHours(
+                      e.target.value,
+                      officeHourBlock.id,
+                      false,
+                    );
+                  }}
+                />
+              </label>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- src/components/SingleAttendeeSection.tsx
+++ src/components/SingleAttendeeSection.tsx
@@ -11,22 +11,29 @@ export function SingleAttendeeSection({
   handleUpdateName,
 }: SingleAttendeeSectionImports) {
   return (
-    <div>
-      <div key={attendee.id} className="attendee-row">
-        <label htmlFor={`${attendee.id}-name`}>
-          Name
-          <input
-            type="text"
-            id={`${attendee.id}-name`}
-            placeholder={`Person ${attendee.name}`}
-            value={attendee.name}
-            onChange={(e) => handleUpdateName(attendee.id, e.target.value)}
-            className="attendee-input"
+    <>
+      <div>
+        <div key={attendee.id} className="attendee-row">
+          <label htmlFor={`${attendee.id}-name`}>
+            Name
+            <input
+              type="text"
+              id={`${attendee.id}-name`}
+              placeholder={`Person ${attendee.name}`}
+              value={attendee.name}
+              onChange={(e) => handleUpdateName(attendee.id, e.target.value)}
+              className="attendee-input"
+            />
+          </label>
+          <LocationInput
+            attendee={attendee}
+            handleUpdateLocation={handleUpdateLocation}
           />
-        </label>
-        <LocationInput
+        </div>
+        <ShareHoursButton
+          fullTeam={false}
+          attendeeArray={null}
           attendee={attendee}
-          handleUpdateLocation={handleUpdateLocation}
         />
       </div>
       <OfficeHoursAdd
@@ -34,7 +41,6 @@ export function SingleAttendeeSection({
         handleAddOfficeHourBlock={handleAddOfficeHourBlock}
         handleOfficeHourChange={handleOfficeHourChange}
       />
-      <ShareHoursButton fullTeam={false} attendeeArray={null} attendee={attendee}/>
-    </div>
+    </>
   );
 }
